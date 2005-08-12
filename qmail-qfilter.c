823f85dd6d62e6669eb2e8d1ce2b074568825e44
Bruce Guenter <bruce@untroubled.org>
2005-08-12 22:39:21 +0000

    Added 2005 to copyright years.

--- qmail-qfilter.c
+++ qmail-qfilter.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004 Bruce Guenter <bruceg@em.ca>
+/* Copyright (C) 2001,2004-2005 Bruce Guenter <bruceg@em.ca>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
